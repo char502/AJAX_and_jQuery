06032456f025e035588156f148823fc118ba9d40
char502 <char502@hotmail.com>
2018-03-04 21:50:47 +0000

    calling some data with AJAX

---
+++ index.js
@@ -0,0 +1,12 @@
+//Ajax request to API with jQuery
+
+// JSON - JavaScript Object notation
+
+$(document).ready(function () {
+    var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
+    $.getJSON(url, function (data) {
+        console.log(data)
+    })
+})
+
+
