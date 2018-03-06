38b37cc17b3628789f78ec76ba37722c79625617
char502 <char502@hotmail.com>
2018-03-06 21:00:53 +0000

    Ajax and jQuery and displaying in the browser

--- index.js
+++ index.js
@@ -5,8 +5,34 @@
 $(document).ready(function () {
     var url = 'http://hn.algolia.com/api/v1/search?query=javascript';
     $.getJSON(url, function (data) {
-        console.log(data)
-    })
-})
+        // console.log(data instanceof Object);
+        // if (Array.isArray(data.hits)) {
+        //     console.log('This is an Array');
+        // } else {
+        //     console.log('This is not an Array')
+        // }
+        var allNews = data.hits; // 20 hits
+        console.log(allNews);
+        var eachNews = '';
+
+        allNews.map(function (item, index, array) {
+            $('.question').on('click', function () {
+                eachNews += "<div>";
+                eachNews += item.title;
+                eachNews += "</div>";
+                $('.answer').html(eachNews);
+            });
+        })
+    });
+});
+
 
 // Ajax response, Object or Array?
+
+// if an Object - use the forIn method, if it is an array might use the map method, therefore it is important to determine the type of data you are working with
+
+
+// Display Ajax response in the browser
+
+
+// Working with Geolocation Data
